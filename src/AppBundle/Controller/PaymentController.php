389eca6dab3d0af635fa54cc3bc4838ef915481a
Konstantin Grachev <ko@grachev.io>
2016-10-01 21:03:44 +0300

    Add Payment::listAction

--- src/AppBundle/Controller/PaymentController.php
+++ src/AppBundle/Controller/PaymentController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Repository\PaymentRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/payment", service="app.controller.payment")
@@ -11,6 +13,31 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
  */
 final class PaymentController extends BaseController
 {
+    /**
+     * @var PaymentRepository
+     */
+    private $paymentRepository;
+
+    /**
+     */
+    public function __construct(PaymentRepository $paymentRepository)
+    {
+        $this->paymentRepository = $paymentRepository;
+    }
+
+    /**
+     * @Route("/", name="payment_list")
+     */
+    public function listAction(Request $request)
+    {
+        $pageSize = 50;
+        $pageIndex = $request->query->get('page', 1);
+
+        return $this->render(':payment:list.html.twig', [
+            'pagerfanta' => $this->paymentRepository->paginateLatest($pageSize, $pageIndex),
+        ]);
+    }
+
     /**
      * @Route("")
      */
--- src/AppBundle/Entity/Repository/PaymentRepository.php
+++ src/AppBundle/Entity/Repository/PaymentRepository.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity\Repository;
 
 use AppBundle\Doctrine\EntityRepository;
 use AppBundle\Entity\Payment;
+use Pagerfanta\Pagerfanta;
 
 /**
  * @author Konstantin Grachev <me@grachevko.ru>
@@ -13,5 +14,19 @@ final class PaymentRepository extends EntityRepository
     protected function getClass(): string
     {
         return Payment::class;
+    }
+
+    /**
+     * @param int $pageSize
+     * @param int $pageIndex
+     *
+     * @return Pagerfanta
+     */
+    public function paginateLatest(int $pageSize, int $pageIndex)
+    {
+        $qb = $this->createQueryBuilder('pt')
+            ->orderBy('pt.id', 'DESC');
+
+        return $this->paginate($qb, $pageSize, $pageIndex);
     }
 }
