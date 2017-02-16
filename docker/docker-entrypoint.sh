7ff3bdb1f7460f22a1b1d1764ba0eb29ca7d3cf7
Konstantin Grachev <me@grachevko.ru>
2017-02-16 17:59:34 +0300

    CS Fix docker-entrypoint.sh and Dockerfile

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -9,66 +9,63 @@ if [ ! -z "$GITHUB_AUTH_TOKEN" ]; then
 fi
 
 # Skip entrypoint if running composer, php or sh
-if echo "$1" | grep -q -E '^(composer|php|sh)$'; then
-	exec "$@"
-
-	exit 0
-fi
-
-case ${SYMFONY_ENV} in
-   prod|dev|test)
-	;;
-   *)
-	>&2 echo env "SYMFONY_ENV" must be in \"prod, dev, test\"
-	exit 1
-	;;
+case "$1" in
+   composer|php|sh) exec "$@" && exit 0;;
 esac
 
-case ${SYMFONY_DEBUG} in
-   0)
-	;;
-   1)
-	touch ${APP_DIR}/web/config.php
-	;;
-   *)
-	>&2 echo env "SYMFONY_DEBUG" must be in \"1, 0\"
-	exit 1
-	;;
+case "$SYMFONY_ENV" in
+   prod|dev|test) ;;
+   *) >&2 echo env "SYMFONY_ENV" must be in \"prod, dev, test\" && exit 1;;
 esac
+
+case "$SYMFONY_DEBUG" in
+   0) ;;
+   1) touch ${APP_DIR}/web/config.php;;
+   *) >&2 echo env "SYMFONY_DEBUG" must be in \"1, 0\" && exit 1;;
+esac
+
+COMMAND="$@"
+COMPOSER_DEFAULT_EXEC=${COMPOSER_DEFAULT_EXEC:="composer install --no-interaction --prefer-dist"}
 
 if [ "$SYMFONY_ENV" == "dev" ]; then
-    COMPOSER_EXEC=${COMPOSER_EXEC:="composer install --no-interaction --optimize-autoloader --prefer-dist --verbose --profile"}
+    COMPOSER_EXEC=${COMPOSER_EXEC:="$COMPOSER_DEFAULT_EXEC --optimize-autoloader --verbose --profile"}
+
     XDEBUG=${XDEBUG:=true}
     OPCACHE=${OPCACHE:=false}
     APCU=${APCU:=false}
 
     COMMAND=${COMMAND:=php-server}
 
-elif [ "$SYMFONY_ENV" == "prod" ]; then
-    COMPOSER_EXEC=${COMPOSER_EXEC:="composer install --no-dev --no-interaction --apcu-autoloader --no-progress --prefer-dist"}
-
-    COMMAND=${COMMAND:=apache}
-
 elif [ "$SYMFONY_ENV" == "test" ]; then
-	COMPOSER_EXEC=${COMPOSER_EXEC:="composer install --no-interaction --optimize-autoloader --no-progress --prefer-dist"}
+    COMPOSER_EXEC=${COMPOSER_EXEC:="$COMPOSER_DEFAULT_EXEC --apcu-autoloader --no-progress"}
+
 	REQUIREMENTS=${REQUIREMENTS:=true}
 	FIXTURES=${FIXTURES:=true}
 
 	COMMAND=${COMMAND:=test}
+
+elif [ "$SYMFONY_ENV" == "prod" ]; then
+    COMPOSER_EXEC=${COMPOSER_EXEC:="$COMPOSER_DEFAULT_EXEC --no-dev --apcu-autoloader --no-progress"}
+
+    COMMAND=${COMMAND:=apache}
 fi
 
 OPCACHE=${OPCACHE:=true}
 APCU=${APCU:=true}
 MIGRATION=${MIGRATION:=true}
 
-ext-enable() {
+enableExt() {
     extension=$1
-    docker-php-ext-enable ${extension}
+    docker-php-enableExt ${extension}
     echo -e " > $extension enabled"
 }
 
 if [ "$OPCACHE" == "true" ]; then
-    ext-enable opcache
+    enableExt opcache
+fi
+
+if [ "$APCU" == "true" ]; then
+    enableExt apcu
 fi
 
 if [ "$COMPOSER_EXEC" != "false" ]; then
@@ -84,11 +81,11 @@ if [ "$FIXTURES" == "true" ]; then
 fi
 
 if [ "$XDEBUG" == "true" ]; then
-    ext-enable xdebug
+    enableExt xdebug
 fi
 
 if [ -f ${APP_DIR}/web/config.php ]; then
 	sed -i "s~'::1',~'::1', '$DOCKER_BRIDGE_IP',~g" "$APP_DIR/web/config.php"
 fi
 
-exec ${COMMAND}
+exec "$COMMAND"
