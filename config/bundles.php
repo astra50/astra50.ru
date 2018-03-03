ad05c633c41cba7c37d3e8f06e04e493337a8293
Konstantin Grachev <me@grachevko.ru>
2018-03-03 14:04:15 +0300

    Update symfony.lock

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 return [
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
