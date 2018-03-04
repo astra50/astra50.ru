7c35edcf571a43fddccd3a326c868b388636ccf6
Konstantin Grachev <me@grachevko.ru>
2018-03-05 01:48:34 +0300

    Arrears: Fix arrears query

--- src/Controller/ArrearsController.php
+++ src/Controller/ArrearsController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Area;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -24,22 +26,27 @@ final class ArrearsController extends Controller
               purpose.id AS purpose_id,
               purpose.name AS purpose_name,
               area.number AS area_number,
-              GROUP_CONCAT(DISTINCT u.realname) AS owners,
               SUM(payment.amount) AS payment_amount
             FROM purpose
               LEFT JOIN payment ON purpose.id = payment.purpose_id
               LEFT JOIN area ON payment.area_id = area.id
-              LEFT JOIN area_user a ON area.id = a.area_id
-              LEFT JOIN users u ON a.user_id = u.id
             WHERE purpose.archived_at IS NULL
             GROUP BY purpose.id, area.id
             ORDER BY ABS(area.number)
 SQL
         );
 
+        /** @var Area[] $areas */
+        $areas = $em->createQueryBuilder()
+            ->select('area')
+            ->from(Area::class, 'area', 'area.number')
+            ->getQuery()
+            ->getResult();
+
         $purposes = [];
-        $rows = array_map(function ($row) use (&$purposes) {
+        $rows = array_map(function ($row) use (&$purposes, $areas) {
             $purposes[$row['purpose_id']] = $row['purpose_name'];
+            $owners = $areas[$row['area_number']]->getUsers();
 
             return [
                 'purpose' => [
@@ -48,7 +55,9 @@ SQL
                 ],
                 'area' => [
                     'number' => $row['area_number'],
-                    'owners' => explode(',', $row['owners'] ?? ''),
+                    'owners' => $owners ? implode(', ', array_map(function (User $user) {
+                        return $user->getRealname();
+                    }, $owners)) : null,
                 ],
                 'amount' => $row['payment_amount'],
             ];
